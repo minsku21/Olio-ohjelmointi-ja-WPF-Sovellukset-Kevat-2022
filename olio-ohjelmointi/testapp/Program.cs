491cc1ec56e60984b45674507e06c9f1b6fa0254
Minnea Simonsson <minnea.simonsson@student.careeria.fi>
2022-01-10 12:58:50 +0200

    koira esimerkki

--- olio-ohjelmointi/testapp/Program.cs
+++ olio-ohjelmointi/testapp/Program.cs
@@ -6,7 +6,21 @@ namespace testapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            koira koira1 = new koira();
+            koira1.nimi = "Tiitus";
+            koira1.ikä = 4;
+            koira1.rotu = "Mainecoon";
+
+            koira1.Mauku();
+            koira1.TulosData();
+
+            koira koira2 = new koira();
+
+            koira2.nimi = "Vinski";
+            koira2.ikä = 9;
+            koira2.rotu = "Ragdoll";
+
+            koira2.TulosData();
         }
     }
 }
---
+++ olio-ohjelmointi/testapp/koira.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testapp
+{
+    class koira
+    {
+        public string nimi;
+        public int ikä;
+        public string rotu;
+
+        public void TulosData()
+        {
+            Console.WriteLine("koiran nimi: " + nimi + ". ikä: " + ikä + ". Rotu: " + rotu);
+        }
+        public void Mauku()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
